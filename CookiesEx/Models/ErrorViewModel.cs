14ac0c8ed5b2379267f8183e91d45d75f9d066c4
123Boryana <glavchevaboryana@gmail.com>
2024-11-25 08:33:35 +0200

    Add project files.

---
+++ CookiesEx/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CookiesEx.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
